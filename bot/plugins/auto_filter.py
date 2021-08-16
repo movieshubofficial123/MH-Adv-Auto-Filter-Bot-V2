e66634fc4e2794a68ef246a8f0eff38e9b11a0b0
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-08-16 22:37:58 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,6 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                Photo="https://telegra.ph/file/00a988145f1ca33dd7152.jpg",
                 text=f"Found {(len_results)} Results For Your Request » <code>{query}</code> ✓",
                 reply_markup=reply_markup,
                 parse_mode="html",
