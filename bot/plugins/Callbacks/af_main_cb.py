2f7a6d75cb424b3dc0e96849f30b0d5a964d5065
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-08-17 17:36:12 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -119,7 +119,7 @@ async def cb_navg(bot, update: CallbackQuery):
                     [
                         InlineKeyboardButton
                             (
-                                f"〽️ New Release 〽️", url=invite_link
+                                f"〽️ New Release 〽️", url='https://t.me/MH_Newrelease1'
                             )
                     ]
                 )
@@ -128,7 +128,7 @@ async def cb_navg(bot, update: CallbackQuery):
                 ibuttons[-1].append(
                     InlineKeyboardButton
                         (
-                            f"⚡️ MH Series ⚡️", url=invite_link
+                            f"⚡️ MH Series ⚡️", url='https://t.me/MH_Newrelease1'
                         )
                 )
             
