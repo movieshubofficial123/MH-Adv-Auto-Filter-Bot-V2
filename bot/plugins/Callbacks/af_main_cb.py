cb0ac2f74272b778ec7a0c8b0c313d46636a288d
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-08-17 08:11:39 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -119,7 +119,7 @@ async def cb_navg(bot, update: CallbackQuery):
                     [
                         InlineKeyboardButton
                             (
-                                f"〽️ New Release 〽️", url ='https://t.me/MH_Newrelease1'),
+                                f"〽️ New Release 〽️", url ='https://t.me/MH_Newrelease1',
                             )
                     ]
                 )
