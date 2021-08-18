f94381b02d27758929a4736f37fd376cf8f1c9f4
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-08-18 09:22:27 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -139,7 +139,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
    
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Request »</i> <code><b><u>{query}</b></u></code>"
+    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Request </i>»<b> <code>{query}</code></b>"
         
     try:
         await update.message.edit(
